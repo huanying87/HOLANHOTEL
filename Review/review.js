a45f2e1d137d0f3c5a1e70845a9a60abfbd36a76
huanying87 <145742236+huanying87@users.noreply.github.com>
2023-09-25 20:52:49 +0800

    Delete Review directory

--- Review/review.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    // Toggle the answer when the question is clicked
-    $('.c-iconfonticon').click(function() {
-        $(this).closest('.list-item').find('.userReview').slideToggle();
-    });
-});
