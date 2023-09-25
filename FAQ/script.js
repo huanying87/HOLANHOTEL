abd3142b0b9db7558497fb3c1ea48fa0207722b1
huanying87 <145742236+huanying87@users.noreply.github.com>
2023-09-25 20:51:28 +0800

    Delete FAQ directory

--- FAQ/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    // Toggle the answer when the question is clicked
-    $('.question').click(function() {
-        $(this).closest('.list-item').find('.answer').slideToggle();
-    });
-});
