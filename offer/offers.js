b22048d86189d8bba3f0f37b27e65e8d71d44980
huanying87 <145742236+huanying87@users.noreply.github.com>
2023-09-25 21:04:42 +0800

    Add files via upload

---
+++ offer/offers.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // JavaScript code here
+    document.getElementById("mealButton").addEventListener("click", function () {
+        alert("You have selected the Meal Package!");
+    });
+
+    document.getElementById("discount75Button").addEventListener("click", function () {
+        alert("You have selected the 75% Discount Package!");
+    });
+
+    document.getElementById("discount50Button").addEventListener("click", function () {
+        alert("You have selected the 50% Discount Package!");
+    });
+
+    document.getElementById("discount25Button").addEventListener("click", function () {
+        alert("You have selected the 25% Discount Package!");
+    });
+});
